7fe2526f6642944fd682deabfe5eaec193695605
Administrator <gitlab_admin_1de235@example.com>
2025-07-03 10:51:04 +0800

    refactor: 修改打包输出路径 新增打包命令

--- src/config/common.js
+++ src/config/common.js
@@ -3,78 +3,81 @@ const env = process.env.NODE_ENV;
 console.log(" env ===> ", env);
 
 let serverDomain = "http://118.64.0.1";
-let answerSever = '';
-let taikeServer = '';
-let appServer = '';
+let answerSever = "";
+let taikeServer = "";
+let appServer = "";
 let publicPath = "/";
 switch (env) {
-    // 国信私有云内网
-    case "relasebcos":
-        serverDomain = "https://1hao.bonc.local";
-        answerSever = "https://1hao.bonc.local/",
-        taikeServer = "http://apitikr.bonc.local/";
-        appServer = "https://1hao.bonc.local/";
-        publicPath = "/";
-        break;
-    // 长城网预测试
-    case "k8stest":
-        serverDomain = "https://backend.soap.com";
-        answerSever = "https://backend.soap.com/",
-        taikeServer = "https://cms-api.soap.com/";
-        appServer = "https://backend.soap.com/";
-        publicPath = "/";
-        break;
-    // 长城网预生产
-    case "k8spre":
-        serverDomain = "https://appusercent.81.mil.cn";
-        answerSever = "https://appusercent.81.mil.cn/";
-        taikeServer = "";
-        appServer = "https://appusercent.81.mil.cn";
-        publicPath = "/";
-        break;
-    // 长城网生产
-    case "k8sproduction":
-        serverDomain = "https://apparmy.81.mil.cn";
-        answerSever = "https://apptracker.81.mil.cn/"
-        taikeServer = "https://cms-api.81.mil.cn";
-        appServer = "https://apparmy.81.mil.cn";
-        publicPath = "/";
-        break;
-    default:
-        // serverDomain = "https://backend.soap.com";
-        // answerSever = "https://backend.soap.com/",
-        // taikeServer = "https://cms-api.soap.com/";
-        // appServer = "https://backend.soap.com/";
-        // publicPath = "/";
-		serverDomain = "https://apparmy.81.mil.cn";
-		answerSever = "https://apptracker.81.mil.cn/"
-		taikeServer = "https://cms-api.81.mil.cn";
-		appServer = "https://apparmy.81.mil.cn";
-		publicPath = "/";
-        break;
+  // 国信私有云内网
+  case "relasebcos":
+    serverDomain = "https://1hao.bonc.local";
+    (answerSever = "https://1hao.bonc.local/"), (taikeServer = "http://apitikr.bonc.local/");
+    appServer = "https://1hao.bonc.local/";
+    publicPath = "/";
+    break;
+  // 长城网预测试
+  case "k8stest":
+    serverDomain = "https://backend.soap.com";
+    (answerSever = "https://backend.soap.com/"), (taikeServer = "https://cms-api.soap.com/");
+    appServer = "https://backend.soap.com/";
+    publicPath = "/";
+    break;
+  // 长城网预生产
+  case "k8spre":
+    serverDomain = "https://appusercent.81.mil.cn";
+    answerSever = "https://appusercent.81.mil.cn/";
+    taikeServer = "";
+    appServer = "https://appusercent.81.mil.cn";
+    publicPath = "/";
+    break;
+  // 长城网生产
+  case "k8sproduction":
+    serverDomain = "https://apparmy.81.mil.cn";
+    answerSever = "https://apptracker.81.mil.cn/";
+    taikeServer = "https://cms-api.81.mil.cn";
+    appServer = "https://apparmy.81.mil.cn";
+    publicPath = "/";
+    break;
+  case "test":
+    serverDomain = "http://192.168.100.44:8080";
+    serverDomainApp = "http://192.168.100.44:8080";
+    publicPath = "/";
+    break;
+  default:
+    // serverDomain = "https://backend.soap.com";
+    // answerSever = "https://backend.soap.com/",
+    // taikeServer = "https://cms-api.soap.com/";
+    // appServer = "https://backend.soap.com/";
+    // publicPath = "/";
+    serverDomain = "https://apparmy.81.mil.cn";
+    answerSever = "https://apptracker.81.mil.cn/";
+    taikeServer = "https://cms-api.81.mil.cn";
+    appServer = "https://apparmy.81.mil.cn";
+    publicPath = "/";
+    break;
 }
 
 const baseConfig = {
-    PUBLIC_PATH: publicPath,
-    SERVER_DOMIN: serverDomain,
-    ANSWER_SEVER:answerSever,
-    TAIKR_SERVER: taikeServer,
-    APP_SERVER: appServer,
-    SERVER_NAME: {
-        vote:'vote',
-        system: "user-sys",
-        sys: "sys", // 部门管理
-        cms: "cms", //cms
-        strongArmy: "strongArmy",
-        credits: serverDomain + "/cms-app/credits",
-        special: serverDomain + "/cms-app/special",
-        course: "course-app", //课程号
-        cmsApp: "cms-app",
-        answer:'bonc-answer-app',
-        im:'im'
-    }
+  PUBLIC_PATH: publicPath,
+  SERVER_DOMIN: serverDomain,
+  ANSWER_SEVER: answerSever,
+  TAIKR_SERVER: taikeServer,
+  APP_SERVER: appServer,
+  SERVER_NAME: {
+    vote: "vote",
+    system: "user-sys",
+    sys: "sys", // 部门管理
+    cms: "cms", //cms
+    strongArmy: "strongArmy",
+    credits: serverDomain + "/cms-app/credits",
+    special: serverDomain + "/cms-app/special",
+    course: "course-app", //课程号
+    cmsApp: "cms-app",
+    answer: "bonc-answer-app",
+    im: "im",
+  },
 };
 
 module.exports = {
-    baseConfig
+  baseConfig,
 };
--- vue.config.js
+++ vue.config.js
@@ -8,91 +8,86 @@ const { baseConfig } = require("./src/config/common");
 const Version = dayjs().format("YYYYMMDD-HHmmss") + "-v2.0.1";
 
 function resolve(dir) {
-    return path.join(__dirname, dir);
+  return path.join(__dirname, dir);
 }
 
 const port = process.env.port || process.env.npm_config_port || 7001; // dev port
 
 module.exports = {
-    publicPath: baseConfig.PUBLIC_PATH || "/",
-    outputDir: "dist",
-    assetsDir: "static",
-    lintOnSave: process.env.NODE_ENV === "development",
-    productionSourceMap: false,
-    devServer: {
-        port: port,
-        open: true,
-        overlay: {
-            warnings: false,
-            errors: true
-        },
-        stats: {
-            builtAt: true,
-            timings: true
-        },
-        proxy: {
-            // 本地代理解决跨域，设置serverDomain = ""
-            '/cms-app': {
-                target: 'https://apparmy.81.mil.cn',
-                secure: false,
-                ws: true,
-                changeOrigin: true
-            },
-        
-        },
+  publicPath: baseConfig.PUBLIC_PATH || "/",
+  outputDir: "h5-dist",
+  assetsDir: "static",
+  lintOnSave: process.env.NODE_ENV === "development",
+  productionSourceMap: false,
+  devServer: {
+    port: port,
+    open: true,
+    overlay: {
+      warnings: false,
+      errors: true,
     },
-    configureWebpack: {
-        devtool: 'inline-source-map',
-        resolve: {
-            alias: {
-                "@": resolve("src")
-            }
-        },
-        output: {
-            filename: `js/[name].${Version}.js`,
-            chunkFilename: `js/[name].${Version}.js`
-        },
-        externals: {
-            // "dayjs": "dayjs"
-        }
+    stats: {
+      builtAt: true,
+      timings: true,
     },
-    chainWebpack(config) {
-        config.optimization.splitChunks({
-            minChunks: 3,
-            cacheGroups: {
-                vendor: {
-                    name: 'chunk-vendor',
-                    chunks: 'initial',
-                    test({ resource }) {
-                        return /[\\/]node_modules[\\/](vue|vue-router|axios)/.test(resource);
-                    },
-                    enforce: true,
-                    reuseExistingChunk: true,
-                    priority: 3,
-                },
-                vantUI: {
-                    name: 'chunk-vant-ui',
-                    chunks: 'initial',
-                    enforce: true,
-                    reuseExistingChunk: true,
-                    priority: 2,
-                    test: /[\\/]node_modules[\\/]vant[\\/]/,
-                }
-            }
-        })
-        config.plugin("preload").tap(() => [
-            {
-                rel: "preload",
-                fileBlacklist: [
-                    /\.map$/,
-                    /hot-update\.js$/,
-                    /runtime\..*\.js$/
-                ],
-                include: "initial",
-            }
-        ]);
+    proxy: {
+      // 本地代理解决跨域，设置serverDomain = ""
+      "/cms-app": {
+        target: "https://apparmy.81.mil.cn",
+        secure: false,
+        ws: true,
+        changeOrigin: true,
+      },
+    },
+  },
+  configureWebpack: {
+    devtool: "inline-source-map",
+    resolve: {
+      alias: {
+        "@": resolve("src"),
+      },
+    },
+    output: {
+      filename: `js/[name].${Version}.js`,
+      chunkFilename: `js/[name].${Version}.js`,
+    },
+    externals: {
+      // "dayjs": "dayjs"
+    },
+  },
+  chainWebpack(config) {
+    config.optimization.splitChunks({
+      minChunks: 3,
+      cacheGroups: {
+        vendor: {
+          name: "chunk-vendor",
+          chunks: "initial",
+          test({ resource }) {
+            return /[\\/]node_modules[\\/](vue|vue-router|axios)/.test(resource);
+          },
+          enforce: true,
+          reuseExistingChunk: true,
+          priority: 3,
+        },
+        vantUI: {
+          name: "chunk-vant-ui",
+          chunks: "initial",
+          enforce: true,
+          reuseExistingChunk: true,
+          priority: 2,
+          test: /[\\/]node_modules[\\/]vant[\\/]/,
+        },
+      },
+    });
+    config.plugin("preload").tap(() => [
+      {
+        rel: "preload",
+        fileBlacklist: [/\.map$/, /hot-update\.js$/, /runtime\..*\.js$/],
+        include: "initial",
+      },
+    ]);
 
-        // when there are many pages, it will cause too many meaningless requests
-        config.plugins.delete("prefetch");
-    }
+    // when there are many pages, it will cause too many meaningless requests
+    config.plugins.delete("prefetch");
+  },
 };
